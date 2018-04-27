bb61a70e44a520429524f28e6ff8d9e537829f47
colg <121529654@qq.com>
2018-04-27 23:29:09 +0800

    add > spring-boot

---
+++ spring-boot/spring-boot-01-helloworld/src/main/java/cn/colg/SpringBoot01HelloworldApplication.java
@@ -0,0 +1,15 @@
+package cn.colg;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Spring Boot 启动类
+ *
+ * @author colg
+ */
+public class SpringBoot01HelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01HelloworldApplication.class, args);
+    }
+}
