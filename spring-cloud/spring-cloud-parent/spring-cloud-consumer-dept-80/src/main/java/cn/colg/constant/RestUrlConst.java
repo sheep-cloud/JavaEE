3d292d728444d99948a008b2af517c1b79395e42
colg <121529654@qq.com>
2018-04-30 04:17:21 +0800

    delete > @Deprecated restTemplate

--- spring-cloud/spring-cloud-parent/spring-cloud-consumer-dept-80/src/main/java/cn/colg/constant/RestUrlConst.java
+++ spring-cloud/spring-cloud-parent/spring-cloud-consumer-dept-80/src/main/java/cn/colg/constant/RestUrlConst.java
@@ -9,7 +9,11 @@ public final class RestUrlConst {
 
     private RestUrlConst() {}
 
-    /** 部门服务 restTemplate (http://ip:port) */
+    /**
+     * 部门服务 restTemplate (http://ip:port)
+     * 
+     * @deprecated 2018-04-30 此常量已经启用，使用ribbon性能更好，{@link #DEPT_RIBBON_URL_PREFIX}
+     */
     @Deprecated
     public static final String DEPT_REST_TEMPLATE_URL_PREFIX = "http://localhost:8001";
 
