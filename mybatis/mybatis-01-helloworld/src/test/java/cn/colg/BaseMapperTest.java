47eb773ebf7706319e3a3c8dbe75ad7239f23219
colg <121529654@qq.com>
2018-05-01 19:46:49 +0800

    add > mybatis-01-helloworld :: mybatis helloworld

---
+++ mybatis/mybatis-01-helloworld/src/test/java/cn/colg/BaseMapperTest.java
@@ -0,0 +1,49 @@
+package cn.colg;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+
+/**
+ * Mapper 测试基类
+ *
+ * @author colg
+ */
+public abstract class BaseMapperTest {
+
+    public static final Log log = LogFactory.get();
+
+    /** 获取sqlSsession工厂 */
+    private static SqlSessionFactory sqlSessionFactory;
+    /** 获取sqlSession实例，提供给子类 */
+    protected SqlSession sqlSession;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // 从 XML 中构建SqlSessionFactory
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // 从SqlsessionFactory 中获取 sqlSession
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // 销毁sqlSession
+        sqlSession.close();
+        log.info("tearDown() : {}", "----------------------------------------------------------------------------------------------------");
+    }
+}
