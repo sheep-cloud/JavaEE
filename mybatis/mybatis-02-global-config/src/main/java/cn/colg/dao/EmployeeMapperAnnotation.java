12468bc22b76c613d5c683c861a6e6478ae4d0de
colg <121529654@qq.com>
2018-05-01 19:54:53 +0800

    add > mybatis-02-global-config :: 全局配置文件

--- mybatis/mybatis-02-global-config/src/main/java/cn/colg/dao/EmployeeMapperAnnotation.java
+++ mybatis/mybatis-02-global-config/src/main/java/cn/colg/dao/EmployeeMapperAnnotation.java
@@ -5,8 +5,19 @@ import org.apache.ibatis.annotations.Select;
 
 import cn.colg.entity.Employee;
 
+/**
+ * 员工MapperAnnotation
+ *
+ * @author colg
+ */
 public interface EmployeeMapperAnnotation {
 
+    /**
+     * 根据id查询员工
+     *
+     * @param id
+     * @return
+     */
     @Select("SELECT * FROM tbl_employee WHERE id = #{id}")
     Employee findById(@Param("id") Integer id);
 }
--- mybatis/mybatis-02-global-config/src/test/java/cn/colg/dao/EmployeeMapperTest.java
+++ mybatis/mybatis-02-global-config/src/test/java/cn/colg/dao/EmployeeMapperTest.java
@@ -1,58 +1,26 @@
 package cn.colg.dao;
 
-import java.io.InputStream;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cn.colg.BaseMapperTest;
 import cn.colg.entity.Employee;
-import cn.hutool.log.Log;
-import cn.hutool.log.LogFactory;
 
 /**
  * 员工Mapper 测试
  *
  * @author colg
  */
-public class EmployeeMapperTest {
+public class EmployeeMapperTest extends BaseMapperTest {
 
-    public static final Log log = LogFactory.get();
-
-    private static SqlSession session;
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        // 从 XML 中构建 SqlSessionFactory
-        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-        // 从 SqlSessionFactory 中获取 SqlSession
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        session = sqlSessionFactory.openSession();
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        session.close();
-    }
-
+    /**
+     * Test method for {@link cn.colg.dao.EmployeeMapper#findById(java.lang.Integer)}.
+     */
     @Test
-    public void testFindById01() throws Exception {
+    public void testFindById() {
         // mybatis会为接口自动创建一个代理对象，代理对象去执行增删改查方法
-        EmployeeMapper employeeMapper = session.getMapper(EmployeeMapper.class);
+        EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
         Employee employee = employeeMapper.findById(1);
-        log.info("testFindById01() >> employee : {}", employee);
-    }
-
-    @Test
-    public void testFindById02() throws Exception {
-        // mybatis会为接口自动创建一个代理对象，代理对象去执行增删改查方法
-        EmployeeMapperAnnotation employeeMapperAnnotation = session.getMapper(EmployeeMapperAnnotation.class);
-        Employee employee = employeeMapperAnnotation.findById(1);
-        log.info("testFindById02() >> employee : {}", employee);
+        log.info("testFindById() >> employee : {}", employee);
     }
 
 }
