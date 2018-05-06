11d9e8c8679622ac01cc6b1bc717ff2123c77dd6
colg <121529654@qq.com>
2018-05-06 18:21:43 +0800

    add > spring-annotation-01-config :: Spring 注解驱动开发

--- spring-annotation/spring-annotation-01-config/README.md
+++ spring-annotation/spring-annotation-01-config/README.md
@@ -4,3 +4,4 @@
 1. 包扫描+组件注解（@Controller/@Service/@Repository/@Component）[自己写的类]
 2. @Bean[导入的第三方包里面的组件]
 3. @Import[快速给容器中导入一个组件]
+4. 使用Spring提供的FactoryBean（工厂Bean）
--- spring-annotation/spring-annotation-01-config/src/main/java/cn/colg/bean/Person.java
+++ spring-annotation/spring-annotation-01-config/src/main/java/cn/colg/bean/Person.java
@@ -4,15 +4,15 @@ import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Person实体
  *
  * @author colg
  */
-@NoArgsConstructor
+@Slf4j
 @AllArgsConstructor
 @Data
 @Accessors(chain = true)
@@ -22,4 +22,16 @@ public class Person implements Serializable {
 
     private String name;
     private Integer age;
+
+    public Person() {
+        log.info("Person() >>  : {}", "Person 构造方法");
+    }
+
+    public void init() {
+        log.info("init() >>  : {}", "Person init 初始化方法");
+    }
+
+    public void destroy() {
+        log.info("destroy() >>  : {}", "Person destroy 销毁方法");
+    }
 }
