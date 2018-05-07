3b9bf95367cd066285da8de14a647f0e467ef0b9
colg <121529654@qq.com>
2018-05-07 23:07:09 +0800

    add > spring-boot-03-config :: Spring Boot 配置文件

--- spring-boot/spring-boot-03-config/src/main/java/cn/clog/SpringBoot03ConfigApplication.java
+++ spring-boot/spring-boot-03-config/src/main/java/cn/clog/SpringBoot03ConfigApplication.java
@@ -3,6 +3,11 @@ package cn.clog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot 启动类
+ *
+ * @author colg
+ */
 @SpringBootApplication
 public class SpringBoot03ConfigApplication {
 
