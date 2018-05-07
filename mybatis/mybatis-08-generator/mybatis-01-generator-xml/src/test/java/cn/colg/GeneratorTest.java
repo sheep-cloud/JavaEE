2a32b26db36e64fc8770eaed2be28cff7616601d
colg <121529654@qq.com>
2018-05-08 01:37:46 +0800

    add > mybatis-08-generator :: mybatis 代码生成器

---
+++ mybatis/mybatis-08-generator/mybatis-01-generator-xml/src/test/java/cn/colg/GeneratorTest.java
@@ -0,0 +1,31 @@
+package cn.colg;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * mybatis generator 代码生成器
+ *
+ * @author colg
+ */
+public class GeneratorTest {
+
+    @Test
+    public void testGenerator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("src/main/resources/generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
