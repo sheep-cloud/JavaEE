77308805644294a204767cd5758a27da714e9fdf
colg <121529654@qq.com>
2018-09-29 23:49:26 +0800

    update -> spring-boot

--- spring-boot/spring-boot-07-web-restful-crud/src/main/java/cn/colg/SpringBoot07WebRestfulCrudApplication.java
+++ spring-boot/spring-boot-07-web-restful-crud/src/main/java/cn/colg/SpringBoot07WebRestfulCrudApplication.java
@@ -3,6 +3,11 @@ package cn.colg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spirng Boot 启动类
+ *
+ * @author colg
+ */
 @SpringBootApplication
 public class SpringBoot07WebRestfulCrudApplication {
 
