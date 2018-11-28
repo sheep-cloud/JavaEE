fcc02404f812b11995db920ff7210e26f41377bf
colg <121529654@qq.com>
2018-11-29 01:11:02 +0800

    add -> 容联 云通讯 SDK

---
+++ spring-boot/CCP_REST_SDK_JAVA_v2.7r1/demo/src/main/java/com/cloopen/rest/demo/QuerySMSTemplate.java
@@ -0,0 +1,40 @@
+package com.cloopen.rest.demo;
+
+import java.util.HashMap;
+import java.util.Set;
+import com.cloopen.rest.sdk.CCPRestSDK;
+import com.cloopen.rest.sdk.CCPRestSDK.BodyType;
+
+
+
+public class QuerySMSTemplate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HashMap<String, Object> result = null;
+
+		CCPRestSDK restAPI = new CCPRestSDK();
+		restAPI.init("app.cloopen.com", "8883");// 初始化服务器地址和端口，格式如下，服务器地址不需要写https://
+		restAPI.setAccount("8aaf0708674db3ee016756141538068f", "3e2a23787b844b65ab2dc60bfb27076b");// 初始化主帐号和主帐号TOKEN
+		restAPI.setAppId("8aaf0708674db3ee0167561415930695");// 初始化应用ID
+		result = restAPI.QuerySMSTemplate("1");//模板Id，不带此参数查询全部可用模板 
+
+		System.out.println("QuerySMSTemplate result=" + result);
+		
+		if("000000".equals(result.get("statusCode"))){
+			//正常返回输出data包体信息（map）
+			HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
+			Set<String> keySet = data.keySet();
+			for(String key:keySet){
+				Object object = data.get(key);
+				System.out.println(key +" = "+object);
+			}
+		}else{
+			//异常返回输出错误码和错误信息
+			System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
+		}
+	}
+
+}
