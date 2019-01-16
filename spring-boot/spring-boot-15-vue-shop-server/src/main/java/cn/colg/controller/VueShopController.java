c8bcb4decec950e3343e9fa0432ffcc580cf8aa4
colg <121529654@qq.com>
2019-01-17 07:27:13 +0800

    add -> CookieUtil

--- spring-boot/spring-boot-15-vue-shop-server/src/main/java/cn/colg/controller/VueShopController.java
+++ spring-boot/spring-boot-15-vue-shop-server/src/main/java/cn/colg/controller/VueShopController.java
@@ -4,9 +4,10 @@ import static cn.colg.util.ResultVoUtil.fail;
 import static cn.colg.util.ResultVoUtil.success;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 
-import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -33,6 +34,7 @@ import cn.hutool.captcha.ICaptcha;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.lang.Dict;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.http.HttpUtil;
@@ -43,8 +45,8 @@ import lombok.extern.slf4j.Slf4j;
  *
  * @author colg
  */
-@CrossOrigin
 @Slf4j
+@CrossOrigin
 @RestController
 public class VueShopController {
 
@@ -63,6 +65,7 @@ public class VueShopController {
      */
     @GetMapping("/position/{geohash}")
     public ResultVo position(@PathVariable String geohash) {
+        log.info("/position/{geohash}; geohash: {}", geohash);
         String data = HttpUtil.get("http://cangdu.org:8001/v2/pois/" + geohash);
         return success(JSON.parse(data));
     }
@@ -75,6 +78,7 @@ public class VueShopController {
      */
     @GetMapping("/index_category")
     public ResultVo index_category() {
+        log.info("/index_category");
         String data = ResourceUtil.readUtf8Str("data/index_category.json");
         return success(JSON.parse(data));
     }
@@ -89,7 +93,7 @@ public class VueShopController {
      */
     @GetMapping("/shops")
     public ResultVo shops(@RequestParam String latitude, @RequestParam String longitude) {
-        log.info("/shops, longitude：{}, latitude：{}", longitude, latitude);
+        log.info("/shops; longitude: {}, latitude: {}", longitude, latitude);
         String data = ResourceUtil.readUtf8Str("data/shops.json");
         return success(JSON.parse(data));
     }
@@ -104,17 +108,23 @@ public class VueShopController {
      */
     @GetMapping("/search_shops")
     public ResultVo search_shops(@RequestParam String geohash, @RequestParam String keyword) {
-        HashMap<String, Object> paramMap = new HashMap<>();
-        paramMap.put("extras[]", "restaurant_activity");
-        paramMap.put("type", "search");
-        paramMap.put("geohash", geohash);
-        paramMap.put("keyword", keyword);
+        log.info("/search_shops; geohash: {}, keyword: {}", geohash, keyword);
+        /*
+        Dict paramMap = Dict.create()
+                            .set("extras[]", "restaurant_activity")
+                            .set("type", "search")
+                            .set("geohash", geohash)
+                            .set("keyword", keyword);
+        */
+        Dict paramMap = Dict.create()
+                            .set("geohash", geohash)
+                            .set("keyword", keyword);
         String data = HttpUtil.get("http://cangdu.org:8001/v4/restaurants", paramMap);
         return success(JSON.parse(data));
     }
 
     /**
-     * 5、获取一次性验证码
+     * 5、获取一次性图形验证码
      *
      * @return
      * @author colg
@@ -122,14 +132,14 @@ public class VueShopController {
      */
     @GetMapping("/captcha")
     public void captcha(HttpServletResponse response, HttpSession session) throws IOException {
-        ICaptcha iCaptcha = CaptchaUtil.createLineCaptcha(100, 37, 4, 15);
-        ServletOutputStream out = response.getOutputStream();
+        ICaptcha iCaptcha = CaptchaUtil.createLineCaptcha(100, 37, 4, 0);
+        OutputStream out = response.getOutputStream();
         iCaptcha.write(out);
         IoUtil.close(out);
 
         // 保存到session
         session.setAttribute("captcha", iCaptcha.getCode().toUpperCase());
-        log.info("/captcha, 验证码：{}", session.getAttribute("captcha"));
+        log.info("/captcha; captcha: {}", session.getAttribute("captcha"));
     }
 
     /**
@@ -142,10 +152,9 @@ public class VueShopController {
      * @author colg
      */
     @PostMapping("/login_pwd")
-    public ResultVo login_pwd(@RequestBody String params, HttpSession session) {
+    public ResultVo login_pwd(@RequestBody String params, HttpSession session, HttpServletRequest request) {
         JSONObject jsonObject = JSON.parseObject(params);
-        String name = jsonObject.getString("name");
-        String pwd = SecureUtil.md5(jsonObject.getString("pwd"));
+        log.info("/login_pwd; params: {}", params);
         String captcha = jsonObject.getString("captcha").toUpperCase();
 
         // 可以对用户名/密码格式进行检查, 如果非法, 返回提示信息
@@ -154,6 +163,9 @@ public class VueShopController {
         }
         // 删除保存的验证码
         session.removeAttribute("captcha");
+        
+        String name = jsonObject.getString("name");
+        String pwd = SecureUtil.md5(jsonObject.getString("pwd"));
 
         // 根据用户名查询用户
         Query query = new Query(Criteria.where("name").is(name));
@@ -188,10 +200,9 @@ public class VueShopController {
     public ResultVo sendcode(@RequestParam String phone, HttpSession session) {
         // 生成验证码，6位随机数
         String code = RandomUtil.randomNumbers(6);
-        log.info("向{}发送验证码短信: {}", phone, code);
+        log.info("/sendcode; phone: {}, code: {}", phone, code);
         // 发送给指定的手机号
-        boolean success = SmsUtil.sendCode(phone, code);
-        if (success) {
+        if (SmsUtil.sendCode(phone, code)) {
             // 保存验证码
             users.put("code", code);
             return success(code);
@@ -209,12 +220,11 @@ public class VueShopController {
      * @author colg
      */
     @PostMapping("/login_sms")
-    public ResultVo login_sms(@RequestBody String params,
-                              HttpSession session) {
+    public ResultVo login_sms(@RequestBody String params, HttpSession session) {
+        log.info("/login_sms, params: {}", params);
         JSONObject jsonObject = JSON.parseObject(params);
-        String phone = jsonObject.getString("phone");
         String code = jsonObject.getString("code");
-        log.info("/login_sms: {}, {}", phone, code);
+        
         if (!code.equals(users.get("code"))) {
             return fail("手机号或验证码不正确！");
         }
@@ -222,6 +232,8 @@ public class VueShopController {
         // 删除保存的code
         users.remove("code");
 
+        String phone = jsonObject.getString("phone");
+        
         User user = mongoTemplate.findOne(new Query(Criteria.where("phone").is(phone)), User.class);
         if (user != null) {
             // 保存用户到session
@@ -246,21 +258,22 @@ public class VueShopController {
      */
     @GetMapping("/userinfo")
     public ResultVo userinfo(HttpSession session) {
+        log.info("/userinfo; sessionId: {}", session.getId());
         Object o = session.getAttribute("userid");
         if (o == null) {
             return fail("请先登录！");
         }
         String userid = o.toString();
-        log.info("/userinfo : {}", userid);
+        log.info("/userinfo; userid: {}", userid);
 
         // 查询用户
         User user = mongoTemplate.findOne(new Query(Criteria.where("_id").is(userid)), User.class);
-        if (user != null) {
-            return success(user);
-        } else {
+        if (user == null) {
             // 清除浏览器保存的userid的cookie
             session.removeAttribute("userid");
             return fail("请先登录！");
+        } else {
+            return success(user);
         }
     }
 
@@ -273,8 +286,9 @@ public class VueShopController {
      */
     @GetMapping("/logout")
     public ResultVo logout(HttpSession session) {
+        log.info("/logout; {}", DateUtil.now());
         session.removeAttribute("userid");
-        log.info("/logout: {}", DateUtil.now());
         return success(null);
     }
+    
 }
