111835c783c96bc7f7ecc046b719923b60b609d8
colg <yanglei@puyiwm.com>
2019-07-04 17:26:57 +0800

    add -> spring-boot-advance-01-cache

--- spring-boot-advance/spring-boot-advance-01-cache/src/main/resources/sql/data-spring_boot_advance.sql
+++ spring-boot-advance/spring-boot-advance-01-cache/src/main/resources/sql/data-spring_boot_advance.sql
@@ -1,4 +1,14 @@
 SELECT * FROM department;
 SELECT * FROM employee;
-SELECT e.* FROM employee e WHERE e.id = ?;
-INSERT INTO employee(lastName, email, gender, d_id) VALUES(?, ?, ?, ?);
+
+INSERT INTO
+    department(departmentName)
+VALUES
+    ('技术部'),
+    ('测试部');
+    
+INSERT INTO 
+	employee(lastName, email, gender, d_id)
+	VALUES
+		('jack', 'jack@qq.com', 0, 1),
+		('rose', 'rose@qq.com', 1, 2);
